fea84126f2eb7286216e65e9ca1eed4da0804f69
Nate Hansen <77819611+nateute@users.noreply.github.com>
2021-12-04 15:27:49 -0700

    updated header comments on i2c and open_drain. WIP on fss_top and ext_mem

--- src/v/rtl/ext/peripheral/i2c_bus.sv
+++ src/v/rtl/ext/peripheral/i2c_bus.sv
@@ -1,6 +1,16 @@
 //
+// University of Utah, Computer Design Laboratory ECE 3710, FSSPrototype
 //
+// Create Date: 12/4/2021
+// Module Name: i2c_bus
+// Description: A wrapper for a pair of open-drain configured busses to be used with i2c.
+// Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
 //
+
+// @param I_SDA_T     tri-state signal for the SDA line.
+// @param I_SCL_T     tri-state signal for the SCL line.
+// @param O_SDA       data line for the SDA signal.
+// @param O_SCL       data line for the SCL signal.
 
 module i2c_bus(input I_SDA_T,
                input I_SCL_T,
--- src/v/rtl/ext/peripheral/open_drain.sv
+++ src/v/rtl/ext/peripheral/open_drain.sv
@@ -1,4 +1,14 @@
+//
+// University of Utah, Computer Design Laboratory ECE 3710, FSSPrototype
+//
+// Create Date: 12/4/2021
+// Module Name: open_drain
+// Description: An open-drain configured tristate bus.
+// Authors: Jacob Peterson, Brady Hartog, Isabella Gilman, Nate Hansen
+//
 
+// @param I_T     tri-state signal
+// @param O_DATA  data line
 module open_drain ( input I_T,
                     output O_DATA);
 						  
